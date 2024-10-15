eab39de85382a23f6b23248526edf30975c107a7
pfb2024 <pfb2024@info07.cshl.edu>
2024-10-15 16:00:52 -0400

    adding statement use

---
+++ python2/statement_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+time= 3
+if time > 10:
+  message= 'go to sleep, is too late'
+  print(f'Now is {time}, so {message}')
+else:
+  message='you should be practicing python'
+  print(f'Now is {time}, so {message}')
