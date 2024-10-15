7ac491c2f76193c02bb5a007dd66198bab325893
pfb2024 <pfb2024@info07.cshl.edu>
2024-10-15 11:26:05 -0400

    added name to print

---
+++ python1/about_me.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+name= sys.argv[1]
+favcolor= sys.argv[2]
+favactivity=sys.argv[3]
+favanimal=sys.argv[4]
+print(f"My names:{name} \n My favorite color is {favcolor} and my favorite activity is {favactivity}. \n My favorite animala is {favanimal}")
+
