edc87ff9226b056b0a10a142d10f457f2cd5fc59
pfb2024 <pfb2024@info07.cshl.edu>
2024-10-18 21:19:05 -0400

    updating the regex usage

--- python7/regex_p7_8.py
+++ python7/regex_p7_8.py
@@ -5,11 +5,11 @@
 # curl -O https://raw.githubusercontent.com/prog4biol/pfb2024/master/files/Python_07_ApoI.fasta
 import re
 
-# --- Problem 7
+# --- Problem 8
 #R = A or G 
 #Y = C or T
 
-
+fragments = []
 with open('Python_07_ApoI.fasta', "r") as fasta_file:
     for line in fasta_file:
         line = line.rstrip()
@@ -18,13 +18,20 @@ with open('Python_07_ApoI.fasta', "r") as fasta_file:
             before=cut_site.group(1)
             after=cut_site.group(2)
             newline = re.sub(r'([AG])AATT([CT])',r'\1^AATT\2', line)
-            print(newline)
+            fragments+=newline.split('^')
+print(fragments)
+print(sorted(fragments, key=len))
+
+
+# Creating an empty dictonary
+len_segmets = {}
+
+# builting the dictionary
+for frag in fragments:
+    len_segmets[len(frag)]= frag
+   
+#print(len_segmets)
 
 
 
 
-# with open('Python_07_ApoI.fasta', "r") as fasta_file:
-#     for line in fasta_file:
-#         line = line.rstrip()
-#         cut_site = re.search(r'(A|G)AATT(C|T)', line)
-#         print(cut_site)
